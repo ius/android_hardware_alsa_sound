7060b3af1e510901dc9b554265bba15920c9634c
Joerie de Gram <j.de.gram@gmail.com>
2010-12-27 02:10:07 +0100

    Add missing LOCAL_MODULE_TAGS
    Change-Id: I27566bd47b2102c6e391d51283f7f7a336f6692b

--- Android.mk
+++ Android.mk
@@ -23,6 +23,7 @@ ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
 	ALSAControl.cpp
 
   LOCAL_MODULE := libaudio
+  LOCAL_MODULE_TAGS := optional
 
   LOCAL_STATIC_LIBRARIES += libaudiointerface
 
@@ -54,6 +55,7 @@ endif
   LOCAL_SRC_FILES := AudioPolicyManagerALSA.cpp
 
   LOCAL_MODULE := libaudiopolicy
+  LOCAL_MODULE_TAGS := optional
 
   LOCAL_WHOLE_STATIC_LIBRARIES += libaudiopolicybase
 
@@ -87,6 +89,7 @@ endif
   	liblog
 
   LOCAL_MODULE:= alsa.default
+  LOCAL_MODULE_TAGS := optional
 
   include $(BUILD_SHARED_LIBRARY)
 
@@ -107,6 +110,7 @@ endif
   LOCAL_SHARED_LIBRARIES := liblog
 
   LOCAL_MODULE:= acoustics.default
+  LOCAL_MODULE_TAGS := optional
 
   include $(BUILD_SHARED_LIBRARY)
 
